9553ce82bd65fd02e71649c6d71f7aa14c875c36
visualstudio <visualstudio@122-PC40>
2019-03-06 11:34:33 -0500

    lab 11

--- lab 12/Program.cs
+++ lab 12/Program.cs
@@ -1,15 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace lab_12
+//student name yogesh yogesh
+// student id c0722784
+//csd3354 Section 2
+//Assignment 2
+// march 6,2019
+namespace DelegatesAndEvents
 {
+    using System;
+
+    delegate void ExampleDelegate(string xyz);
+
     class Program
     {
-        static void Main(string[] args)
+        public static void Method1(string xyz)
         {
+            Console.WriteLine(xyz + " Method1");
+        }
+
+        public static void Method2(string xyz)
+        {
+            Console.WriteLine(xyz + " Method2");
+        }
+
+        public static void Main()
+        {
+            ExampleDelegate ex1Delegate, ex2Delegate, ex3Delegate, myDelegate;
+
+            ex1Delegate = new ExampleDelegate(Method1);
+            ex2Delegate = new ExampleDelegate(Method2);
+            ex3Delegate = ex1Delegate + ex2Delegate;
+            myDelegate = ex1Delegate - ex2Delegate;
+            ex1Delegate("AAA");
+            ex2Delegate("BBB");
+            ex3Delegate("CCC");
+            myDelegate("DDD");
+            myDelegate = ex3Delegate - ex1Delegate;
+            myDelegate("EEE");
+            myDelegate = ex3Delegate - ex2Delegate;
+            myDelegate("FFF");
+            Console.ReadLine();
         }
     }
 }
